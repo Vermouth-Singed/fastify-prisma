2247894bf3dcae0b8fbfc9e0d34491f2dbd3574d
Vermouth-Singed <qhddusl2004@naver.com>
2026-06-26 10:22:16 +0900

    Update crypto.ts

--- src/config/crypto.ts
+++ src/config/crypto.ts
@@ -1,16 +1,30 @@
-const crypto = require('crypto');
+import {
+    createHash,
+    createDecipheriv
+} from 'crypto';
 
-const secretKey = crypto.createHash('sha256')
-    .update(process.env.SECRET_KEY)
+const secretKey = createHash('sha256')
+    .update(process.env.SECRET_KEY!)
     .digest();
 
-function decrypt(encryptedText) {
+export function decrypt(encryptedText: string): string {
     const [ivHex, encrypted] = encryptedText.split(':');
+
     const iv = Buffer.from(ivHex, 'hex');
-    const decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, iv);
-    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+
+    const decipher = createDecipheriv(
+        'aes-256-cbc',
+        secretKey,
+        iv
+    );
+
+    let decrypted = decipher.update(
+        encrypted,
+        'hex',
+        'utf8'
+    );
+
     decrypted += decipher.final('utf8');
+
     return decrypted;
 }
-
-module.exports = { decrypt };
